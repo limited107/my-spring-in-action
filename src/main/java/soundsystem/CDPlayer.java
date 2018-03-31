7610441e781b0a8b397086a708d8186cf6772411
cole.c <cole.c@kakaocorp.com>
2018-03-31 15:46:47 +0900

    @Autowired로 생성자, setter에 주입 가능

---
+++ src/main/java/soundsystem/CDPlayer.java
@@ -0,0 +1,19 @@
+package soundsystem;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CDPlayer implements MediaPlayer {
+	private CompactDisc cd;
+
+	// 스프링이 빈을 인스턴스화한 이후에는 @Autowired로 애너테이트된 setCompactDisc() 같은 메소드를 통해 종속 관계를 만족시킨다
+	@Autowired
+	public CDPlayer(CompactDisc cd) {
+		this.cd = cd;
+	}
+
+	public void play() {
+		cd.play();
+	}
+}
